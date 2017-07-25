970ee714dfec1789b4252e1f4bfb468af3db6dde
Daniel <daniel.hjartstrom@gmail.com>
2017-07-25 22:55:40 +0200

    Initial commit

---
+++ DownloadTest/DownloadTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DownloadTest
+//
+//  Created by Daniel Hjärtström on 2017-07-25.
+//  Copyright © 2017 Daniel Hjärtström. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
