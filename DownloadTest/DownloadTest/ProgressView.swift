99d37277b3d70362ee609ec2e7ed302e43eb44a9
Daniel <daniel.hjartstrom@gmail.com>
2017-07-26 20:40:37 +0200

    Working animation

--- DownloadTest/DownloadTest/ProgressView.swift
+++ DownloadTest/DownloadTest/ProgressView.swift
@@ -15,7 +15,7 @@ class ProgressView: UIView {
     func initiateDownload(){
         
         shapeLayer = CAShapeLayer()
-        shapeLayer?.lineWidth = 9.0
+        shapeLayer?.lineWidth = 10.0
         shapeLayer?.fillColor = UIColor.clear.cgColor
         shapeLayer?.strokeColor = UIColor.green.cgColor
         shapeLayer?.lineCap = kCALineCapRound
@@ -24,8 +24,13 @@ class ProgressView: UIView {
         
         let radius = self.frame.width * 0.2
         
-        let circlePath = UIBezierPath(arcCenter:CGPoint(x: self.frame.width / 2, y: self.frame.height / 2), radius:radius, startAngle: CGFloat(Double.pi) * 3.0, endAngle:CGFloat(Double.pi) * 3.0 + CGFloat(Double.pi) * 2.0, clockwise: true)
-
+        //let startAngle: CGFloat = CGFloat(3.0 * Double.pi / 4)
+        //let endAngle: CGFloat = CGFloat(Double.pi / 4.0)
+        
+        let startAngle = CGFloat((Double.pi / 2) * 3)
+        let endAngle = CGFloat(Double.pi * 2 + (Double.pi / 2) * 3)
+        
+        let circlePath = UIBezierPath(arcCenter:CGPoint(x: self.frame.width / 2, y: self.frame.height / 2), radius:radius, startAngle: startAngle, endAngle:endAngle, clockwise: true)
         
         shapeLayer?.path = circlePath.cgPath
         
@@ -36,7 +41,7 @@ class ProgressView: UIView {
     func updateStroke(progress: CGFloat){
     
         DispatchQueue.main.async {
-         
+            
            self.shapeLayer?.strokeEnd = progress
             
         }
